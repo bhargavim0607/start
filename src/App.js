fa5d266f8e0b4c54f204432d4ce60e6f8a9c6aed
bhargavim0607 <bhargavim0607@gmail.com>
2021-05-20 19:04:48 +0530

    deploy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
+import Testinomial from './components/test';
+import Header from './components/header';
+import Service from './components/service';
+import Pricing from './components/pricing';
+import Subscribe from './components/subscribe';
+import Contact from './components/contact.jsx';
+import Footer from './components/footer.jsx';
+import './components/assets/css/style.css';
+import './components/assets/css/bootstrap.min.css'
+import './components/assets/css/default.css'
+import './components/assets/css/LineIcons.css'
+import './components/assets/css/magnific-popup.css'
+import './components/assets/css/style.css'
+import './components/assets/css/style.css.map'
+import './components/assets/js/main';
 import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+       <Header/>
+       <Service/>
+       <Pricing/>
+       <Subscribe/>
+       <Testinomial/>
+       <Contact/>
+       <Footer/>
+    
+</div>
   );
 }
 
---
+++ src/components/test.jsx
@@ -0,0 +1,146 @@
+import React from 'react';
+
+
+function test(){
+    return( <section id="test" class="test">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-6">
+                <div class="section-title text-center pb-10">
+                    <h4 class="title">Testinomial</h4>
+                    <p class="text">Stop wasting time and money designing and managing a website that doesn’t get results. Happiness guaranteed!</p>
+                </div> 
+            </div>
+        </div>
+        <div id="demo" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators" >
+                <li data-target="#demo" data-slide-to="0" class="active"></li>
+                <li data-target="#demo" data-slide-to="1"></li>
+                <li data-target="#demo" data-slide-to="2"></li>
+                <li data-target="#demo" data-slide-to="3"></li>
+            </ol>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <div class="row">
+                    <div class="col-md-4 mb-3">
+                        <div class="card">
+                            <img class="img" style={{borderRadius:"100%", width: "fitContent",height:" fit-content",marginLeft:" 25%"}} alt="100%x280" src="https://preview.uideck.com/items/start/assets/images/author-3.jpg"/>
+                            <div class="card-body">
+                                
+                                <p class="card-text">Stop wasting time and money designing and managing a website that doesn't get results.Happiness guaranteed! Stop wasting time and money designing and managing a website that doesn't get results.Happiness guaranteed!</p>
+                                <hr/>
+                                <h4 class="card-title" >Isabela Moreria</h4>
+                                <p class="r" >CEO, GrayGrids</p>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <div class="card">
+                            <img class="img" style={{borderRadius:"100%", width: "fitContent",height:" fit-content",marginLeft:" 25%"}} alt="100%x280" src="https://preview.uideck.com/items/start/assets/images/author-1.jpg"/>
+                            <div class="card-body">
+                                <p class="card-text">Stop wasting time and money designing and managing a website that doesn't get results.Happiness guaranteed! Stop wasting time and money designing and managing a website that doesn't get results.Happiness guaranteed! </p>
+                                <hr/>
+                                <h4 class="card-title">FIONA</h4>
+                                <p class="r">Lead Designer, UIdeck</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <div class="card">
+                            <img class="img" style={{borderRadius:"100%", width: "fitContent",height:" fit-content",marginLeft:" 25%"}} alt="100%x280" src="https://preview.uideck.com/items/start/assets/images/author-2.jpg"/>
+                            <div class="card-body">
+                                <p class="card-text">Stop wasting time and money designing and managing a website that doesn't get results.Happiness guaranteed! Stop wasting time and money designing and managing a website that doesn't get results.Happiness guaranteed!</p>
+                                <hr/>
+                                <h4 class="card-title">Elon Musk</h4>
+                                <p class="r">CEO,SpaceX</p>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>   
+              </div>
+              <div class="carousel-item">
+                <div class="row">
+                    <div class="col-md-4 mb-3">
+                        <div class="card">
+                            <img class="img" style={{borderRadius:"100%", width: "fitContent",height:" fit-content",marginLeft:" 25%"}} alt="100%x280" src="https://preview.uideck.com/items/start/assets/images/author-1.jpg"/>
+                            <div class="card-body">
+                                <p class="card-text">Stop wasting time and money designing and managing a website that doesn't get results.Happiness guaranteed! Stop wasting time and money designing and managing a website that doesn't get results.Happiness guaranteed! </p>
+                                <hr/>
+                                <h4 class="card-title">FIONA</h4>
+                                <p class="r">Lead Designer, UIdeck</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <div class="card">
+                            <img class="img" style={{borderRadius:"100%", width: "fitContent",height:" fit-content",marginLeft:" 25%"}} alt="100%x280" src="https://preview.uideck.com/items/start/assets/images/author-2.jpg"/>
+                            <div class="card-body">
+                                <p class="card-text">Stop wasting time and money designing and managing a website that doesn't get results.Happiness guaranteed! Stop wasting time and money designing and managing a website that doesn't get results.Happiness guaranteed!</p>
+                                <hr/>
+                                <h4 class="card-title">Elon Musk</h4>
+                                <p class="r">CEO,SpaceX</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <div class="card">
+                            <img class="img" style={{borderRadius:"100%", width: "fitContent",height:" fit-content",marginLeft:" 25%"}} alt="100%x280" src="https://preview.uideck.com/items/start/assets/images/author-4.jpg"/>
+                            <div class="card-body">
+                                <p class="card-text">Stop wasting time and money designing and managing a website that doesn't get results.Happiness guaranteed! Stop wasting time and money designing and managing a website that doesn't get results.Happiness guaranteed!</p>
+                                <hr/>
+                                <h4 class="card-title">Fajar Siddiq</h4>
+                                <p class="r">CEO,MakerFlix</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>     
+              </div>
+              <div class="carousel-item">
+                <div class="row">
+                    <div class="col-md-4 mb-3">
+                        <div class="card">
+                            <img class="img" style={{borderRadius:"100%", width: "fitContent",height:" fit-content",marginLeft:" 25%"}} alt="100%x280" src="https://preview.uideck.com/items/start/assets/images/author-2.jpg"/>
+                            <div class="card-body">
+                                <p class="card-text">Stop wasting time and money designing and managing a website that doesn't get results.Happiness guaranteed! Stop wasting time and money designing and managing a website that doesn't get results.Happiness guaranteed!</p>
+                                <hr/>
+                                <h4 class="card-title">Elon Musk</h4>
+                                <p class="r">CEO,SpaceX</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <div class="card">
+                            <img class="img" style={{borderRadius:"100%", width: "fitContent",height:" fit-content",marginLeft:" 25%"}} alt="100%x280" src="https://preview.uideck.com/items/start/assets/images/author-4.jpg"/>
+                            <div class="card-body">
+                                <p class="card-text">Stop wasting time and money designing and managing a website that doesn't get results.Happiness guaranteed! Stop wasting time and money designing and managing a website that doesn't get results.Happiness guaranteed!</p>
+                                <hr/>
+                                <h4 class="card-title">Fajar Siddiq</h4>
+                                <p class="r">CEO,MakerFlix</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <div class="card">
+                            <img class="img" style={{borderRadius:"100%", width: "fitContent",height:" fit-content",marginLeft:" 25%"}} alt="100%x280" src="https://preview.uideck.com/items/start/assets/images/author-3.jpg"/>
+                            <div class="card-body">
+                                <p class="card-text">Stop wasting time and money designing and managing a website that doesn't get results.Happiness guaranteed! Stop wasting time and money designing and managing a website that doesn't get results.Happiness guaranteed!</p>
+                                <hr/>
+                                <h4 class="card-title">Isabela Moreria</h4>
+                                <p class="r">CEO, GrayGrids</p>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>      
+              </div>
+            </div>
+           
+          </div>
+
+    </div>
+</section>);
+}
+
+export default test;
